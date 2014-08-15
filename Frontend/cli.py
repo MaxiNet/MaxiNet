b95584d89e0d2a04380a51de35a8c1cf797d1da8
Felix Wallaschek <felix.wallaschek@upb.de>
2014-08-15 16:07:12 +0200

    set xauthority of root to maxinet user

--- Frontend/cli.py
+++ Frontend/cli.py
@@ -113,7 +113,8 @@ class CLI(Cmd):
         else:
             pid = self.experiment.get_worker(self.experiment.get(node)).run_cmd("ps ax | grep \"bash -ms mininet:"+node+"\" -m1 | awk '{print $1}'").strip()
             if self.experiment.get_worker(self.experiment.get(node)).tunnelX11(node):
-                rcmd = "ssh -q -X -t "+self.experiment.get_worker(self.experiment.get(node)).hn()+" sudo mnexec -a "+pid+" "+cmd
+                user = subprocess.check_output("ssh -q -t "+self.experiment.get_worker(self.experiment.get(node)).hn()+" echo $USER", shell=True).strip()
+                rcmd = "ssh -q -X -t "+self.experiment.get_worker(self.experiment.get(node)).hn()+" sudo  XAUTHORITY=/home/"+user+"/.Xauthority mnexec -a "+pid+" "+cmd
             else:
                 rcmd = "ssh -q -t "+self.experiment.get_worker(self.experiment.get(node)).hn()+" sudo mnexec -a "+pid+" "+cmd
             subprocess.call(rcmd,shell=True)
