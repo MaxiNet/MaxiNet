1af97b29519d974ae9e9da27c5e3ac773c1d415d
Felix Wallaschek <felix.wallaschek@upb.de>
2014-08-26 15:54:00 +0200

    added tun module to modules which have to be loaded

--- Worker/bin/load_tunneling.sh
+++ Worker/bin/load_tunneling.sh
@@ -5,6 +5,7 @@ then
         sudo rmmod openvswitch
         sudo rmmod openvswitch_mod
         sudo modprobe ip_gre
+        sudo modprobe tun
         sudo modprobe openvswitch
         sudo modprobe openvswitch_mod
         echo "done"
