MaxiNet/MaxiNet

commit 8da0820f9c8df33869b87777a688a285250da24e

Never return a MAC that has the highest bit set  as MAC (fixes #26)

MaxiNet/tools.py
@@ -254,8 +254,8 @@ class SSH_Tool(object):
 # Fat-tree topology implemention for mininet
 #
 class FatTree(Topo):
-    def randByte(self):
-        return hex(random.randint(0, 255))[2:]
+    def randByte(self, max=255):
+        return hex(random.randint(0, max))[2:]
 
     def makeMAC(self, i):
         return "00:" + self.randByte() + ":" + \
@@ -319,7 +319,7 @@ class Tools(object):
 
     @staticmethod
     def makeMAC(i):
-        return Tools.randByte() + ":" + Tools.randByte() + ":" +\
+        return Tools.randByte(127) + ":" + Tools.randByte() + ":" +\
                Tools.randByte() + ":00:00:" + hex(i)[2:]
 
     @staticmethod

commit 6ce596c019b3a6ea82539b6c8de0d3197ce54188

Fixed minor bug introduced by 8da0820f9c8df33869b87777a688a285250da24e
Signed-off-by: Malte Splietker <malte.splietker@gmail.com>

MaxiNet/tools.py
@@ -314,8 +314,8 @@ class Tools(object):
         return os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + os.sep
 
     @staticmethod
-    def randByte():
-        return hex(random.randint(0, 255))[2:]
+    def randByte(max=255):
+        return hex(random.randint(0, max))[2:]
 
     @staticmethod
     def makeMAC(i):